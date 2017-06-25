a8487a1f659702e4adfe574d2d9f433534eded4e
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-06-25 20:52:59 +0800

    update .dir-locals.el for nand2tetris dev
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,16 @@
 ;; If you have problem with this `.dir-locals.el' file, please make sure you use
 ;; `Emacs 24' and the `(package-initialize)' is called by your .emacs.d.
-((nil . ((eval . (package-install 'nand2tetris))
-         (eval . (require 'nand2tetris))
-         ;; set the nand2tetris-core-base-dir to current directory
-         (eval . (let ((current-directory (file-name-directory (or load-file-name (buffer-file-name)))))
-                   (setq-local nand2tetris-core-base-dir current-directory))))))
+((nil . ((eval . (set (make-local-variable 'my-nand2tetris-path)
+                      (file-name-directory
+                       (let ((d (dir-locals-find-file ".")))
+                         (if (stringp d) d (car d))))))
+         ;; (eval . (message "nand2tetris project directory set to `%s'." my-nand2tetris-path))
+         (eval . (progn
+                   (package-install 'nand2tetris)
+                   (require 'nand2tetris)
+                   (setq nand2tetris-core-base-dir my-nand2tetris-path)
+                   (add-to-list 'auto-mode-alist
+                                (cons (concat (expand-file-name nand2tetris-core-base-dir) "*\\.hdl")
+                                      'nand2tetris-mode))
+                   ))
+         )))
