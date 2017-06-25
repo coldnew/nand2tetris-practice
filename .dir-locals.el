b5e2bab9173e057c2ec409d6b6e5e6a34bd36c0e
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-06-25 21:17:24 +0800

    .dir-locals.el: update
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,28 @@
 ;; If you have problem with this `.dir-locals.el' file, please make sure you use
 ;; `Emacs 24' and the `(package-initialize)' is called by your .emacs.d.
-((nil . ((eval . (set (make-local-variable 'my-nand2tetris-path)
-                      (file-name-directory
-                       (let ((d (dir-locals-find-file ".")))
-                         (if (stringp d) d (car d))))))
-         ;; (eval . (message "nand2tetris project directory set to `%s'." my-nand2tetris-path))
+((nil . ((tab-width . 4)
          (eval . (progn
                    (package-install 'nand2tetris)
                    (require 'nand2tetris)
-                   (setq nand2tetris-core-base-dir my-nand2tetris-path)
+                   (package-install 'nand2tetris-assembler)
+                   (require 'nand2tetris-assembler)
+                   (setq nand2tetris-core-base-dir
+                         (file-name-directory
+                          (let ((d (dir-locals-find-file ".")))
+                            (if (stringp d) d (car d)))))
+
+                   (setq nand2tetris-tools-dir  (expand-file-name "tools" nand2tetris-core-base-dir))
+                   (setq nand2tetris-hardware-simulator (expand-file-name "HardwareSimulator.sh" nand2tetris-tools-dir))
+                   (setq nand2tetris-assembler (expand-file-name "Assembler.sh" nand2tetris-tools-dir))
+                   (setq nand2tetris-cpu-emulator (expand-file-name "CPUEmulator.sh" nand2tetris-tools-dir))
+                   (setq nand2tetris-jack-compiler (expand-file-name "JackCompiler.sh" nand2tetris-tools-dir))
+                   (setq nand2tetris-text-comparer (expand-file-name "TextComparer.sh" nand2tetris-tools-dir))
+                   (setq nand2tetris-vm-emulator (expand-file-name "VMEmulator.sh" nand2tetris-tools-dir))
                    (add-to-list 'auto-mode-alist
-                                (cons (concat (expand-file-name nand2tetris-core-base-dir) "*\\.hdl")
+                                (cons (concat (expand-file-name nand2tetris-core-base-dir) ".*\\.hdl")
                                       'nand2tetris-mode))
+                   (add-to-list 'auto-mode-alist
+                                (cons (concat (expand-file-name nand2tetris-core-base-dir) ".*\\.asm")
+                                      'nand2tetris-assembler-mode))
                    ))
          )))
